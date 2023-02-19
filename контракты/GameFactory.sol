e96382fc3ca170f8167788689e03a140fb8f2add
lonelynightx <105809555+lonelynightx@users.noreply.github.com>
2023-02-19 16:11:13 +0300

    Update GameFactory.sol

--- контракты/GameFactory.sol
+++ контракты/GameFactory.sol
@@ -1,20 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.17;
 
-import "./GamePlayerVsPlayer.sol";
+import "./GameV1PlayerVsPlayer.sol";
 
 contract GameFactory {
     address private owner;
     uint public createdCount = 0;
 
     event gameCreated(address indexed creator, address game, uint count);
+    
     constructor() {
         owner = msg.sender;
     }
 
     function createGame() public {
         address game;
-        bytes memory bytecode = type(GameV1).creationCode;
+        bytes memory bytecode = type(GameV1PlayerVsPlayer).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(msg.sender, createdCount));
         
         assembly {
